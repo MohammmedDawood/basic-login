56fe40a1c476f0fc6b5ab2101ed95ae704b69e21
Mohammed Dawood <mahmedtony73@gmail.com>
2021-12-15 14:42:32 +0200

    Adding auth-context and useContext hook

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </AuthContextProvider>,
+  document.getElementById('root')
 );
